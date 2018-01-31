54a2fc9deaa5375d1608320eb2e77b68678e950e
joebrew <joebrew@gmail.com>
2018-01-31 17:10:37 +0100

    basic navigation utilities created

---
+++ README.md
@@ -0,0 +1,3 @@
+# Mobile Banking Diagnostic Model Scoring Tool
+
+A web application for benchmarking mobile banking competencies
---
+++ global.R
@@ -0,0 +1,8 @@
+# Libraries
+library(tidyverse)
+
+# Create a dictionary of tab names / numbers
+tab_dict <- data_frame(number = 1:2,
+                       name = c('main',
+                                'about'))
+NUM_PAGES <- 2
